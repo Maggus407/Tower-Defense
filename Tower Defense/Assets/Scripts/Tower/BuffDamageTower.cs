d3b15fe4e090c41f88f2432110224ffbda59ce56
Markus Kauschmann <markus.kauschmann@stud.uni-hannover.de>
2023-01-22 18:50:10 +0100

    test?

---
+++ Tower Defense/Assets/Scripts/Tower/BuffDamageTower.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffDamageTower : MonoBehaviour
+{
+    private void Update()
+    {
+        //Get all Tower on the Map with Layer 6
+        //Check if Tower is in range
+        //If Tower is in range, buff Tower
+        
+
+
+    }
+}
